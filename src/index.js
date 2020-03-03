526a07661b8483c35c7df9c8f3a3f534bca8d1a0
Yuri <yury971@mail.ru>
2020-03-03 14:21:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let newbracketsConfig = [], check_string = str;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newbracketsConfig.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < newbracketsConfig.length; j++) {
+      check_string = check_string.replace(newbracketsConfig[j], "");
+    }
+    if (check_string.length == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
